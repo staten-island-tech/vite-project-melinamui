1787c00d4e40a14a9bab12854e5c8e27851cb94a
melinamui <113132371+melinamui@users.noreply.github.com>
2022-12-07 13:57:16 -0500

    finally

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,5 @@
 import "../styles/style.css";
 import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "../counter.js";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
@@ -10,19 +9,7 @@ console.log(menu);
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  <p></p>
   </div>
 `;
 
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,6 +1,31 @@
 const menu = [
   {
-    name: "dim sum",
+    name: "Shrimp Dumplings",
+    chinese_name: "",
+    popular: "yes",
+    vegan: "no",
+    instock: "yes",
+  },
+  {
+    name: "Roasted Pork Rice Noodles",
+    chinese_name: "",
+    popular: "yes",
+    vegan: "no",
+    instock: "yes",
+  },
+  {
+    name: "Chicken Feet",
+    chinese_name: "",
+    popular: "no",
+    vegan: "no",
+    instock: "no",
+  },
+  {
+    name: "Bok Choy",
+    chinese_name: "",
+    popular: "yes",
+    vegan: "yes",
+    instock: "yes",
   },
 ];
 
