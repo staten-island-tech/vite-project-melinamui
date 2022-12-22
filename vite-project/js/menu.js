bfdeb582144b1cf1f6fed78bf35223ef3fd242fc
melinamui <113132371+melinamui@users.noreply.github.com>
2022-12-21 19:38:58 -0500

    Update menu.js

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -15,7 +15,7 @@ const menu = [
     popular: "yes",
     vegan: "no",
     instock: "yes",
-    img: "",
+    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F873557658943967497%2F&psig=AOvVaw1huIK3DI2m4vYXz7vlpAB6&ust=1671755835252000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNDCsff9i_wCFQAAAAAdAAAAABAi",
   },
   {
     show: "all",
@@ -24,6 +24,7 @@ const menu = [
     popular: "no",
     vegan: "no",
     instock: "no",
+    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foxyfolksy.com%2Fchicken-feet-dimsum%2F&psig=AOvVaw3CZGePpE2EcXhJHifSpbXe&ust=1671755892576000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPil_pL-i_wCFQAAAAAdAAAAABAF",
   },
   {
     show: "all",
@@ -32,6 +33,7 @@ const menu = [
     popular: "yes",
     vegan: "yes",
     instock: "yes",
+    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jessicagavin.com%2Fhow-to-cook-bok-choy%2F&psig=AOvVaw3JQQKYqVk8kHugv41cQq4w&ust=1671755923802000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIjr1KH-i_wCFQAAAAAdAAAAABAE",
   },
 ];
 export { menu };
