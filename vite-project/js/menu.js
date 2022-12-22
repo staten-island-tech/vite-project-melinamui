cb9fdc6a0aaa2928200c07508fa08efb77939259
melinamui <113132371+melinamui@users.noreply.github.com>
2022-12-21 19:47:46 -0500

    Update menu.js

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -15,7 +15,7 @@ const menu = [
     popular: "yes",
     vegan: "no",
     instock: "yes",
-    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F873557658943967497%2F&psig=AOvVaw1huIK3DI2m4vYXz7vlpAB6&ust=1671755835252000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNDCsff9i_wCFQAAAAAdAAAAABAi",
+    img: "https://i.pinimg.com/564x/68/56/0a/68560a0e2d12974f2baad9a2c697ffe7.jpg",
   },
   {
     show: "all",
@@ -24,7 +24,7 @@ const menu = [
     popular: "no",
     vegan: "no",
     instock: "no",
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foxyfolksy.com%2Fchicken-feet-dimsum%2F&psig=AOvVaw3CZGePpE2EcXhJHifSpbXe&ust=1671755892576000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPil_pL-i_wCFQAAAAAdAAAAABAF",
+    img:"https://www.foxyfolksy.com/wp-content/uploads/2022/05/chicken-feet-dimsum-1200t.jpg",
   },
   {
     show: "all",
@@ -33,7 +33,7 @@ const menu = [
     popular: "yes",
     vegan: "yes",
     instock: "yes",
-    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jessicagavin.com%2Fhow-to-cook-bok-choy%2F&psig=AOvVaw3JQQKYqVk8kHugv41cQq4w&ust=1671755923802000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIjr1KH-i_wCFQAAAAAdAAAAABAE",
+    img: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2022-07-How-To-Cook-Bok-Choy%2Fbok_choy_2_of_2",
   },
 ];
 export { menu };
