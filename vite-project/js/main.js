e08cd9ad758c48b58637a6f25f659ee13b8a523a
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-03 00:59:23 -0500

    theme

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -12,6 +12,16 @@ console.log(menu);
 //console.log("testW");
 //};
 
+document.querySelector(".theme").addEventListener("click", function () {
+  if (document.body.classList.contains("uwu")) {
+    document.body.classList.add("emo");
+    document.body.classList.remove("uwu");
+  } else {
+    document.body.classList.add("uwu");
+    document.body.classList.remove("emo");
+  }
+});
+
 DOMSelectors.popular.addEventListener("click", function () {
   getPopular();  
 });
