8b9615921ace5a844c260d0a5041d0c38a4deb48
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-03 00:53:29 -0500

    it works!

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,7 +2,6 @@ import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
-import "./functions";
 import { DOMSelectors } from "./dom";
 console.log(DOMSelectors);
 import { menu } from "./menu";
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,6 +1,5 @@
 const menu = [
   {
-    show: "all",
     name: "Shrimp Dumplings",
     chinese_name: "",
     popular: "yes",
@@ -9,7 +8,6 @@ const menu = [
     img: "https://redhousespice.com/wp-content/uploads/2017/09/dim-sum-har-gow-dumplings.jpg",
   },
   {
-    show: "all",
     name: "Roasted Pork Rice Noodles",
     chinese_name: "",
     popular: "yes",
@@ -27,13 +25,19 @@ const menu = [
     img: "https://www.foxyfolksy.com/wp-content/uploads/2022/05/chicken-feet-dimsum-1200t.jpg",
   },
   {
-    show: "all",
     name: "Bok Choy",
     chinese_name: "",
     popular: "yes",
     vegan: "yes",
     instock: "yes",
     img: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2022-07-How-To-Cook-Bok-Choy%2Fbok_choy_2_of_2",
+  },
+  {
+    name: "Shumai",
+    chinese_name: "",
+    popular: "yes",
+    vegan: "no",
+    img: "https://redhousespice.com/wp-content/uploads/2022/11/dim-sum-siu-mai-scaled.jpg",
   },
 ];
 export { menu };
